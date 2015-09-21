03ae8529deeae54b0ad5edebc3bd3322e56d943a
Ron M. Battle <CaptRenaldo@gmail.com>
2015-09-20 18:31:18 -0700

    added batteryLOW function to test voltage

--- firmware/Adafruit_HDC1000.cpp
+++ firmware/Adafruit_HDC1000.cpp
@@ -37,11 +37,12 @@ boolean Adafruit_HDC1000::begin(uint8_t addr) {
 
 
 void Adafruit_HDC1000::reset(void) {
-  // reset, and select 14 bit temp & humidity
+  // reset,combined temp/humidity measurement,  and select 14 bit temp & humidity resolution
   uint16_t config = HDC1000_CONFIG_RST | HDC1000_CONFIG_MODE | HDC1000_CONFIG_TRES_14 | HDC1000_CONFIG_HRES_14;
 
   Wire.beginTransmission(_i2caddr);
-  Wire.write(config>>8);
+  Wire.write(HDC1000_CONFIG);   // set pointer register to configuration register   RMB
+  Wire.write(config>>8);        // now write out 2 bytes MSB first    RMB
   Wire.write(config&0xFF);
   Wire.endTransmission();
   delay(15);
@@ -60,7 +61,7 @@ float Adafruit_HDC1000::readTemperature(void) {
   
 
 float Adafruit_HDC1000::readHumidity(void) {
-  // original code used HDC1000_TEMP register, doesn't work with HDC1000_HUMID  RMB
+  // original code used HDC1000_TEMP register, doesn't work with HDC1000_HUMID with 32bit read  RMB
   float hum = (read32(HDC1000_TEMP, 20) & 0xFFFF);
 
   hum /= 65536;
@@ -69,6 +70,17 @@ float Adafruit_HDC1000::readHumidity(void) {
   return hum;
 }
 
+// Add ability to test battery voltage, useful in remote monitoring, TRUE if <2.8V  
+// Thanks to KFricke for micropython-hdc1008 on GitHub, usually called after Temp/Humid reading  RMB
+bool Adafruit_HDC1000::batteryLOW(void)  {
+  
+  uint16_t battV = (read16(HDC1000_CONFIG_BATT, 20));
+  
+  battV &= HDC1000_CONFIG_BATT;   // mask off other bits
+  
+  return bool battV;
+}  
+  
 
 
 /*********************************************************************/
